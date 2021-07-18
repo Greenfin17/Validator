f0159c6f4216660cae729097d36a9f4a8afa58e3
John Maple <jmaple@bellsouth.net>
2021-07-18 01:34:46 -0500

    validation code in separate class / source file

--- Program.cs
+++ Program.cs
@@ -1,86 +1,10 @@
 ﻿using System;
-using System.Text.RegularExpressions;
+using static ValidationCheck.Validation;
 
 namespace Validator
 {
     class Program
     {
-        static bool IsPinCode(string code)
-        {
-            bool returnVal = true;
-            foreach(char item in code)
-            {
-                if (!char.IsDigit(item))
-                {
-                    returnVal = false;
-                }
-            }
-            if (!(code.Length >= 4 && code.Length <= 8)) {
-                returnVal = false;
-            }
-            return returnVal;
-        }
-        static bool IsPhone(string phone)
-        {
-            bool returnVal = false;
-            Regex phoneRegEx = new Regex(@"^\(?[0-9]{3}\)?[- .]?[0-9]{3}[- .]?([0-9]{4})$");
-            Regex phone555Check = new Regex(@"^\(?[555]\)?");
-            if (phoneRegEx.IsMatch(phone))
-            {
-                returnVal = true;
-            }
-            if (phone555Check.IsMatch(phone))
-            {
-                returnVal = false;
-            }
-            return returnVal;
-        }
-        static bool IsEmail(string email)
-        {
-            bool returnVal = false;
-            email = email.Trim().ToLower();
-            Regex emailRegEx = new Regex(@"^[[a-z.1-9]{1,50}@[a-z1-9]{1,50}[.](com|net|org|edu|mil|gov|edu)$");
-            if (emailRegEx.IsMatch(email))
-            {
-                returnVal = true;
-            }
-            return returnVal;
-        }
-
-        static bool EveryOtherCapitalized(string testString)
-        {
-            bool returnVal = true;
-            bool cont = true;
-            testString = testString.Trim();
-            char[] trimChars = { '!'};
-            testString = testString.TrimEnd(trimChars);
-            int index = 0;
-
-            foreach(char character in testString)
-            {
-                if ( !Char.IsWhiteSpace(character) && ( index == 0 || index %2 == 0 )){
-                    if (!Char.IsLower(character))
-                    {
-                        cont = false;
-                        returnVal = false;
-                    }
-                }
-                else if (!Char.IsWhiteSpace(character) && Char.IsLower(character)){
-                    cont = false;
-                    returnVal = false;
-                }
-                if (!cont)
-                {
-                    break;
-                }
-                // Don't count whitespaces
-                if (!Char.IsWhiteSpace(character))
-                {
-                    index++;
-                }
-            }
-            return returnVal;
-        }
 
         public delegate bool Validation(string input);
 
